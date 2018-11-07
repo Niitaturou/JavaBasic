dcd945a657677d994d873ef6ecbcffc1061639e1
DESKTOP-VMFOSQA\りのん <taturo.nii.likeit@gmail.com>
2018-11-07 16:55:00 +0900

    10-6

--- Practice/src/practice10/PTra10_05.java
+++ Practice/src/practice10/PTra10_05.java
@@ -28,14 +28,20 @@ public class PTra10_05 {
 		// Carクラスを作成後に着手してください
 		// ★ Car型の変数carを宣言し、Carクラスのインスタンスを代入してください
 
+		Car car = new Car();
+
 
 		// ★ 変数carに格納されているインスタンスのserialNoフィールドに、10000を代入してください
 
+		car.serialNo = 10000;
 
 		// ★ 変数carに格納されているインスタンスのcolorフィールドに、"Red"を代入してください
 
+		car.color = "Red";
 
 		// ★ 変数carに格納されているインスタンスのgasolineフィールドに、50を代入してください
+
+		car.gasoline = 50;
 
 
 		// 目的地までの距離
@@ -47,6 +53,34 @@ public class PTra10_05 {
 		 * ★ 目的地についた時点で「目的地にまでn時間かかりました。残りのガソリンは、xリットルです」を出力してください
 		 * ※n：runメソッドを実行した回数, xは残りのガソリンの数です
 		 */
+
+		int sum = 0;
+		int count = 0;
+
+		while( true ) {
+
+			int n = car.run();
+
+			count++;
+
+			if ( n < 0 ) {
+
+				System.out.println( "目的地に到着できませんでした" );
+				break;
+
+			}
+
+			sum += n;
+
+			System.out.println( sum );
+
+			if ( distance < sum ) {
+
+				System.out.println( "目的地にまで" + count + "時間かかりました。残りのガソリンは、" + car.gasoline + "リットルです" );
+				break;
+
+			}
+		}
 
 	}
 }
--- Practice/src/practice10/PTra10_06.java
+++ Practice/src/practice10/PTra10_06.java
@@ -19,5 +19,53 @@ public class PTra10_06 {
 		 * 各carインスタンスのrunメソッドを実行して、それぞれ「目的地にまでn時間かかりました。残りのガソリンは、xリットルです」を出力してください。
 		 */
 
+		Car[] cars = new Car[3];
+
+		Car car = new Car();
+		car.gasoline = 50;
+		car.color = "red";
+
+		Car car1 = new Car();
+		car1.gasoline = 45;
+		car1.color = "blue";
+
+		Car car2 = new Car();
+		car2.gasoline = 40;
+		car2.color = "yellow";
+
+		cars[0] = car;
+		cars[1] = car1;
+		cars[2] = car2;
+
+		final int distance = 300;
+
+		for ( int i = 0; i < cars.length; i++ ) {
+
+			int sum = 0;
+			int count = 0;
+
+			while ( true ) {
+
+				int n = cars[i].run();
+				count++;
+
+				if ( n < 0 ) {
+
+					System.out.println( "目的地に到着できませんでした" );
+					break;
+
+				}
+
+				sum += n;
+
+				if ( distance < sum ) {
+
+					System.out.println( "目的地にまで" + count + "時間かかりました。" +  cars[i].color + "車の残りのガソリンは" + cars[i].gasoline + "リットルです");
+					break;
+
+				}
+			}
+		}
+
 	}
 }

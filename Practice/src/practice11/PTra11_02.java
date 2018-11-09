4613c9aca3955e1678e8ee7765d86a00b90b307b
DESKTOP-VMFOSQA\りのん <taturo.nii.likeit@gmail.com>
2018-11-09 18:31:41 +0900

    12-1

--- Practice/src/practice11/PTra11_02.java
+++ Practice/src/practice11/PTra11_02.java
@@ -11,10 +11,24 @@ public class PTra11_02 {
 
 		// ★ ReadFileClassのクラスメソッドreadBookDataFile()を使い、本情報を取得します
 
+		Book[] bookList = FileReaderClass.readBookDataFile();
+
 
 		System.out.println("探したい本のタイトル（又はその一部）を入力してください");
+		String input = new java.util.Scanner(System.in).nextLine();
 
 		// ★ ユーザが入力した文字列が、本情報のタイトル一部に含まれていた場合は、その本情報を出力してください
+
+		for ( int i = 0, k = 0; i < bookList.length; i++ ) {
+			if ( bookList[i].title.indexOf(input) != -1 ) {
+				System.out.println("--------------------------");
+				k++;
+			}
+		if ( i == (bookList.length - 1 ) && k == 0 ) {
+			System.out.println("お探しの書籍は見つかりませんでした。");
+		}
+
+		}
 
 	}
 }

a7d54d6aee5d64f96e7fdd11a4b7f0b841f07f50
DESKTOP-VMFOSQA\りのん <taturo.nii.likeit@gmail.com>
2018-11-21 16:56:41 +0900

    17-4

--- Practice/src/practice16/PTra16_02.java
+++ Practice/src/practice16/PTra16_02.java
@@ -19,5 +19,15 @@ public class PTra16_02 {
 		double d = 453.592;
 
 		// ★ 上記基本型を、それぞれのラッパークラスに代入してください
+
+		java.lang.Boolean bb = new java.lang.Boolean(b);
+		java.lang.Byte byby = new java.lang.Byte(by);
+		java.lang.Short ss = new java.lang.Short(s);
+		java.lang.Character cc = new java.lang.Character(c);
+		java.lang.Integer ii = new java.lang.Integer(i);
+		java.lang.Long ll = new java.lang.Long(l);
+		java.lang.Float ff = new java.lang.Float(f);
+		java.lang.Double dd = new java.lang.Double(d);
+
 	}
 }
--- Practice/src/practice17/ptra17/PTra17_04.java
+++ Practice/src/practice17/ptra17/PTra17_04.java
@@ -6,15 +6,28 @@
  */
 package practice17.ptra17;
 
+import practice09.util.Util;
+
 public class PTra17_04 {
 	public static void main(String[] args) {
 
-		String str = "スッキリわかるJava入門";
+		try {
+
+			String str = "スッキリわかるJava入門";
+			Util.dispMessageSlowly( str, 500 );
+
+			Thread.sleep( 500 );
+
+		} catch ( InterruptedException e ) {
+			System.out.println( "例外が発生しました" );
+		}
 
 		/*
 		 * ★ strに入っている文字列を1文字ずつゆっくり表示させるプログラムを完成させてください
 		 * ※ Thread.sleep を使用すること
 		 */
+
+
 
 	}
 }
